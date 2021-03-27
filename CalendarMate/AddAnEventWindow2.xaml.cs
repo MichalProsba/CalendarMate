eb159d7bba532752fa22065b2027c5d2123b725f
Student 248986 <248986@student.pwr.edu.pl>
2021-03-28 00:07:38 +0100

    Dodane okno dialogowe

---
+++ CalendarMate/AddAnEventWindow2.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CalendarMate
+{
+    /// <summary>
+    /// Logika interakcji dla klasy AddAnEventWindow2.xaml
+    /// </summary>
+    public partial class AddAnEventWindow2 : Window
+    {
+        public AddAnEventWindow2()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
